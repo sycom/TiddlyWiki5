001f3c0a671c6a1821eb5507fb08f39d3b2e044a
Sylvain Comte <sylvain.comte@yahoo.fr>
2017-01-06 17:11:50 +0100

    having fun trying to send internal tracking

--- plugins/tiddlywiki/googleanalytics/googleanalytics.js
+++ plugins/tiddlywiki/googleanalytics/googleanalytics.js
@@ -29,6 +29,51 @@ exports.startup = function() {
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+	// adding (optional) tracking internal navigation
+	var GA_TRACKALL;
+	if($tw.wiki.getTiddler("$:/GoogleAnalyticsTrackAll")) GA_TRACKALL = $tw.wiki.getTiddlerText("$:/GoogleAnalyticsTrackAll").replace(/\n/g,"");
+	else GA_TRACKALL = "no";
+	if (GA_TRACKALL == "yes") {
+		console.log("tracking-all");
+		/*
+		// enables "permalink" paradsdmeter on navigationadressbar
+		$tw.wiki.setText("$:/config/Navigation/UpdateAddressBar","text","permalink");
+		window.addEventListener("hashchange",function() {
+			var hash = $tw.utils.getLocationHash();
+			ga('set', 'page', '/#'+hash);
+			ga('set', 'title', hash);
+			console.log(hash);
+			ga('send', 'pageview');
+		})*/
+		// create a hook on navigation to send data via tracker
+		$tw.wiki.addEventListener("change",function(changes) {
+			// dealing with user settings !todo check if options is associated with wiki or $tw
+			var options = $tw.wiki.options || {},
+				storyTitle = options.storyTitle || "$:/StoryList",
+				historyTitle = options.historyTitle || "$:/HistoryList";
+			// getting storyList (displayed) historyList (last displayed) and last item
+			var storyList=$tw.wiki.getTiddler(storyTitle).fields.list;
+			var historyList = JSON.parse($tw.wiki.getTiddlerText(historyTitle));
+			var GA_CURRENT = historyList[historyList.length-1].title;
+			// if last item has not been closed, prepare data and send to tracker
+			if(storyList.includes(GA_CURRENT)) {
+				// !todo check if history was modified
+				console.log(changes.historyTitle.modified);
+				console.log('send');
+				}
+			else console.log('don t send');
+			console.log(GA_CURRENT);
+		});
+		/*$tw.rootWidget.addEventListener("tm-navigate",function(event) {
+            console.log("track : "+event.actionTo);
+        });
+		$tw.hooks.addHook("th-track-internal-navigation",function(event) {
+			console.log("track : "+event.navigateTo);
+			return event;
+		});
+		$tw.hooks.invokeHook("th-track-internal-navigation",handleNavigateEvent());*/
+	}
+  // at first connection, should send all default pages to tracker?
   ga('create', GA_ACCOUNT, GA_DOMAIN);
   ga('send', 'pageview');
 };
